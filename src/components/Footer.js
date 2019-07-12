cfe63d722bec99e161cae7529e71cc15af6fb2d7
JhonStark <MrRobot@Jhons-MacBook-Pro.local>
2019-07-12 13:06:18 +0200

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,28 @@ import '../styles/Footer.scss'
 function Footer () {
   return (
     <div>
-      Footer
+      <ul> Contactos
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li>Email: generalservices@gmail.com</li>
+        <li>WhatsApp: 3008626756</li>
+      </ul>
+
+      <ul>
+          <li>Quienes Somos</li>
+          <li>Servicios</li>
+          <li>Pagos</li>
+          <li>Planes</li>
+          <li>Trabaja con Nosotros</li>
+      </ul>
+
+      <ul>Métodos de Pago
+        <li>Bancolombia</li>
+        <li>PSE</li>
+        <li>Efectivo</li>
+      </ul>
     </div>
   )
 }

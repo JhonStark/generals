33cd4cc968660ae3e2a43ec184e16d9ccaeaa75e
JhonStark <MrRobot@Jhons-MacBook-Pro.local>
2019-07-21 16:59:26 +0200

    add router and Link navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,31 @@
 import React from 'react'
 import '../styles/header.scss'
 import logo from '../images/logo.png'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
 
 function Header () {
   return (
+    <Router>
     <div className="container">
       <nav>
+        <ul className="container-logo">
+          <li className="container-logo__logo"><img src={logo} alt="logotipo"/></li>
+          <li className="container-logo__title">GS- General Services</li>
+        </ul>
         <ul className="container-nav">
-          <li className="container-nav__item">
-            <img src={logo} />
+          
+          <li className="container-nav__item--logo">
+            <img src={logo} alt="logotipo"/>
           </li>
-          <li className="container-nav__item">GS- General Services</li>
-          <li className="container-nav__item">boton1</li>
-          <li className="container-nav__item">boton2</li>
-          <li className="container-nav__item">Selecciona el Servicio</li>
+            <li className="container-nav__item--title">GS- General Services</li>
+            <li className="container-nav__item"><Link to="/about/" className="container-nav__item--register">Registro</Link></li>
+            <li className="container-nav__item"><Link to="/about/" className="container-nav__item--register">Ingresar</Link></li>
+            <li className="container-nav__item--service">Selecciona el Servicio</li>
         </ul>
       </nav>
     </div>
+    </Router>
   )
 }
 
